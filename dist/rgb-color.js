c76b2fb00940df577177577bbf88ae472d2f36f5
nmccready <nemtcan@gmail.com>
2018-03-26 11:58:07 -0400

    dist, remove consoles

--- dist/rgb-color.js
+++ dist/rgb-color.js
@@ -168,7 +168,6 @@
     re: /^rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*(\d*\.?\d+)\)$/,
     example: ['rgba(123, 234, 45, .5)', 'rgba(255,234,245, 0.20)'],
     process: function process(bits) {
-      console.log(bits);
       return [parseInt(bits[1], 10), parseInt(bits[2], 10), parseInt(bits[3], 10), +bits[4]];
     }
   }, {
@@ -291,7 +290,6 @@
     }, {
       key: 'hex',
       value: function hex() {
-        console.log(this);
         return '#' + rgbHex(this.r, this.g, this.b, this.a);
       }
     }, {
